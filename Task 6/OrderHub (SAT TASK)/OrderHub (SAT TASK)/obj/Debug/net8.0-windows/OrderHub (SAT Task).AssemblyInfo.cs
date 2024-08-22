699b30423ffae40a53b82462a413764c8d3c39fa
dp2305 <106497046+dp2305@users.noreply.github.com>
2024-08-23 09:27:42 +1000

    update
    unknown this was here for a while.

--- Task 6/OrderHub (SAT TASK)/OrderHub (SAT TASK)/obj/Debug/net8.0-windows/OrderHub (SAT Task).AssemblyInfo.cs
+++ Task 6/OrderHub (SAT TASK)/OrderHub (SAT TASK)/obj/Debug/net8.0-windows/OrderHub (SAT Task).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderHub (SAT Task)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae351fb1f3ee0cceb304ee02cea65c408f9133c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56241ab12b820f5272bcfcd2cd1b71980922f006")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderHub (SAT Task)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderHub (SAT Task)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
